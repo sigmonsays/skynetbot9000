d1e65c39bac3c69e395a77bc805de1a77831d3bd
John Wyles <john@johnwyles.com>
2012-06-06 19:23:51 +0000

    Updating configuration to take a few extra values and adding the skypekit binary (maybe that is a bad idea? oh well.)

--- src/main/java/org/zeroturnaround/Configuration.java
+++ src/main/java/org/zeroturnaround/Configuration.java
@@ -4,6 +4,6 @@ public class Configuration {
   public static String skypeUsername;
   public static String skypePassword;
   public static String pemFile;
-  public static Integer port;
+  public static Integer portNumber;
   public static String postUrl;
 }
